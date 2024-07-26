d976a799cfce577ca0933f958a60e903ac64458c
Dante Menezes <dgdmenezes@gmail.com>
2024-07-25 22:15:33 -0300

    Criação de interface e funcionalidade pra excluir produtos

--- src/app/modules/products/components/products-table/products-table.component.ts
+++ src/app/modules/products/components/products-table/products-table.component.ts
@@ -1,5 +1,6 @@
 import { ProductEvent } from './../../../../models/enums/products/ProductEvent';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { deleteProductAction } from 'src/app/models/interfaces/event/deleteProductAction';
 import { EventAction } from 'src/app/models/interfaces/event/EventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 
@@ -11,6 +12,8 @@ import { GetAllProductsResponse } from 'src/app/models/interfaces/products/respo
 export class ProductsTableComponent {
 @Input() products: Array<GetAllProductsResponse> = [];
 @Output() productEvent = new EventEmitter<EventAction>();
+@Output() deleteProductEvent = new EventEmitter<deleteProductAction>();
+
 public productSelected!: GetAllProductsResponse
 
 public addProductEvent = ProductEvent.ADD_PRODUCT_EVENT
@@ -23,5 +26,12 @@ handleProductEvent(action:string,id?:string):void{
     this.productEvent.emit(productEventData)
   }
 }
-
+handleDeleteProduct(product_id:string, productName:string):void{
+  if(product_id !=="" && productName !=="" ){
+    this.deleteProductEvent.emit({
+      product_id,
+      productName
+    })
+  }
+}
 }
